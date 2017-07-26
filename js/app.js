0681ba66ef71a950c6029ba34f3fab0216028fe8
Oleksandr Yampolskyi <yampolal@gmail.com>
2017-07-26 03:58:07 +0300

    demo-version
    There are some things that I will add soon

---
+++ js/app.js
@@ -0,0 +1,39 @@
+$(function(){
+//For changing items in caruosel in aside
+  var clickEvent = false;
+  $("#m-carousel-st").carousel({
+    interval: 4000
+  }).on('click', '.list-group li', function(){
+    clickEvent = true;
+    $('.list-group li').removeClass('active');
+    $(this).addClass('active');
+  }).on('slide.bs.carousel', function(e){
+    if(!clickEvent){
+      var count = $('.list-group').children().length - 1;
+      var current = $('.list-group li.active');
+      current.removeClass('active').next().addClass('active');
+      var id = parseInt(current.data('slide-to'));
+      if(count == id){
+        $('.list-group li').first().addClass('active');
+      }
+    }
+    clickEvent = false;
+  });
+});
+
+//Set the righ height for items in aside of carousel
+// need refresh if you will resizing browser from medium to small
+$(window).on('load', function() {
+// When screan is 768 and below, something goeng wron
+  if ($(window).width() < 768) {
+   console.log("Return");
+   return;
+}
+  console.log("Dont return");
+  var boxheight = $('#m-carousel-st .carousel-inner').innerHeight();
+  console.log("carousel-st innerHeight = " + boxheight );
+  var itemlength = $('#m-carousel-st .item').length;
+  console.log("Item lemns");
+  var triggerheight = Math.round(boxheight/itemlength+1);
+  $('.list-group-item').outerHeight(triggerheight);
+});
